f46e1d7c40ed9d0629337a234640f9c1de916df7
Divya N <divya2niky@gmail.com>
2023-03-06 13:26:31 -0700

    midway through play.js

--- play.js
+++ play.js
@@ -34,5 +34,63 @@ class Button {
 }
 
 class Game{
+    buttons;
+    allowPlayer;
+    sequence;
+    playerPlaybackPos;
+    mistakeSound;
+
+    constructor(){
+        this.buttons = new Map();
+        this.allowPlayer = false;
+        this.sequence = [];
+        this.playerPlaybackPos = 0;
+        this.mistakeSound = loadSound('error.mp3');
+
+        document.querySelectorAll('.game-button').forEach((el, i) => {
+            if (i < btnDescriptions.length) {
+              this.buttons.set(el.id, new Button(btnDescriptions[i], el));
+            }
+          });
+      
+          const playerNameEl = document.querySelector('.player-name');
+          playerNameEl.textContent = this.getPlayerName();
+        }
+    
+    async pressButton(button){
+        if(this.allowPlayer){
+            this.allowPlayer = false;
+            await this.buttons.get(button.id).press(1.0);
+
+            if(this.sequence[this.playerPlaybackPos].el.id === button.id){
+                this.playerPlaybackPos++;
+                if(this.playerPlaybackPos===this.sequence.length){
+                    this.playerPlaybackPos = 0;
+                    this.addButton();
+                    this.updateScore(this.sequence.length-1);
+                    await this.playSequence();
+                }
+                this.allowPlayer = true;
+            } else {
+                this.saveScore(this.sequence.length - 1);
+                this.mistakeSound.play();
+                await this.buttonDanve(2);
+            }
+        }
+    }
+
+    async reset(){
+        this.allowPlayer = false;
+        this.playerPlaybackPos = 0;
+        this.sequence = [];
+        this.updateScore('--');
+        await this.buttonDance(1);
+        this.addButton();
+        await this.playSequence();
+        this.allowPlayer = true;
+    }
+
+
+
     
 }
